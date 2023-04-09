e35cf2a01b3a98e3709358f6973f6cadbdad363d
ja-me-ch <60724072+ja-me-ch@users.noreply.github.com>
2023-04-09 18:20:34 -0400

    removed unused files

--- helper-functions/previews/LostArkEngravingSimPreview.tsx
+++ helper-functions/previews/LostArkEngravingSimPreview.tsx
@@ -1,11 +1,11 @@
 import Preview from "../../interfaces/preview.interface";
 // import imageUrl from '../../public/previews/LAEngravingSimPreview.png';
-import imageUrl from '../../public/previews/Capture.png';
+// import imageUrl from '../../public/previews/Capture.png';
 
 const LostArkEngravingSimPreview: Preview = {
     title: 'Lost Ark Engraving Simulator',
     name: 'Lost-Ark-Engraving-Simulator-React-',
-    imageUrl: imageUrl,
+    // imageUrl: imageUrl,
     description:
         "A single page application that simulates the engraving system for the game Lost Ark.",
     siteUrl: "https://ja-me-ch.github.io/Lost-Ark-Engraving-Simulator-React-/",
